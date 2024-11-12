93b79545c3a0fdd4aca635d122803477a2a9d2da
Kevin <107502985+kevinn809@users.noreply.github.com>
2024-11-12 14:42:15 -0300

    Add files via upload

--- MySQL/Trabajo Escuela.sql
+++ MySQL/Trabajo Escuela.sql
@@ -42,4 +42,83 @@ create table CicloSuperiorEspecial(
     foreign key(ProfesorCicloSuperiorEspecial) references Profesores(IdProfesor)
 );
 
+
+-- Triggers echo por Kevin Villanueva
+-- Triggers para CicloBasico
+create trigger after_insert_ciclo_basico
+after insert on CicloBasico
+for each row
+insert into Registros (TablaAfectada, Operacion, IdElemento)
+values ('CicloBasico', 'INSERT', NEW.IdCicloBasico);
+
+create trigger after_update_ciclo_basico
+after update on CicloBasico
+for each row
+insert into Registros (TablaAfectada, Operacion, IdElemento)
+values ('CicloBasico', 'UPDATE', NEW.IdCicloBasico);
+
+create trigger after_delete_ciclo_basico
+after delete on CicloBasico
+for each row
+insert into Registros (TablaAfectada, Operacion, IdElemento)
+values ('CicloBasico', 'DELETE', OLD.IdCicloBasico);
+
+-- Triggers para CicloBasicoEspecial
+create trigger after_insert_ciclo_basico_especial
+after insert on CicloBasicoEspecial
+for each row
+insert into Registros (TablaAfectada, Operacion, IdElemento)
+values ('CicloBasicoEspecial', 'INSERT', NEW.IdCicloBasicoEspecial);
+
+create trigger after_update_ciclo_basico_especial
+after update on CicloBasicoEspecial
+for each row
+insert into Registros (TablaAfectada, Operacion, IdElemento)
+values ('CicloBasicoEspecial', 'UPDATE', NEW.IdCicloBasicoEspecial);
+
+create trigger after_delete_ciclo_basico_especial
+after delete on CicloBasicoEspecial
+for each row
+insert into Registros (TablaAfectada, Operacion, IdElemento)
+values ('CicloBasicoEspecial', 'DELETE', OLD.IdCicloBasicoEspecial);
+
+-- Triggers para CicloSuperior
+create trigger after_insert_ciclo_superior
+after insert on CicloSuperior
+for each row
+insert into Registros (TablaAfectada, Operacion, IdElemento)
+values ('CicloSuperior', 'INSERT', NEW.IdCicloSuperior);
+
+create trigger after_update_ciclo_superior
+after update on CicloSuperior
+for each row
+insert into Registros (TablaAfectada, Operacion, IdElemento)
+values ('CicloSuperior', 'UPDATE', NEW.IdCicloSuperior);
+
+create trigger after_delete_ciclo_superior
+after delete on CicloSuperior
+for each row
+insert into Registros (TablaAfectada, Operacion, IdElemento)
+values ('CicloSuperior', 'DELETE', OLD.IdCicloSuperior);
+
+-- Triggers para CicloSuperiorEspecial
+create trigger after_insert_ciclo_superior_especial
+after insert on CicloSuperiorEspecial
+for each row
+insert into Registros (TablaAfectada, Operacion, IdElemento)
+values ('CicloSuperiorEspecial', 'INSERT', NEW.IdCicloSuperiorEspecial);
+
+create trigger after_update_ciclo_superior_especial
+after update on CicloSuperiorEspecial
+for each row
+insert into Registros (TablaAfectada, Operacion, IdElemento)
+values ('CicloSuperiorEspecial', 'UPDATE', NEW.IdCicloSuperiorEspecial);
+
+create trigger after_delete_ciclo_superior_especial
+after delete on CicloSuperiorEspecial
+for each row
+insert into Registros (TablaAfectada, Operacion, IdElemento)
+values ('CicloSuperiorEspecial', 'DELETE', OLD.IdCicloSuperiorEspecial);
+
+
 show tables;
